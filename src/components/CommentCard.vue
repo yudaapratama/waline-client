f9ac132277f247dd7671a84301f0abb1b888aeed
yudaapratama <yudhaprathamaa@gmail.com>
2024-12-19 01:49:56 +0700

    feat: change button action for admin

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -207,17 +207,37 @@ const isEditingCurrent = computed(
 						</button>
 					</div>
 					<div v-if="isAdmin && !isEditingCurrent" class="wl-admin-actions">
-						<span class="wl-comment-status">
-							<button
-								v-for="status in commentStatus"
-								:key="status"
-								type="submit"
-								:class="`wl-btn wl-${status}`"
-								:disabled="comment.status === status"
-								@click="emit('status', { status, comment })"
-								v-text="locale[status]"
-							/>
-						</span>
+						
+						
+							
+						<details class="dropdown">
+								<summary role="button">
+									<a class="button">
+										<svg width="24" height="24" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+											<path d="M7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12Z" fill="#999"/>
+											<path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" fill="#999"/>
+											<path d="M21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12Z" fill="#999"/>
+										</svg>
+									</a>
+								</summary>
+								<ul>
+									<li v-for="status in commentStatus" :key="status">
+										<span
+											:class="{ disabled: comment.status === status }"
+											@click="emit('status', { status, comment })"
+										>{{ locale[status] }}</span>
+									</li>
+							</ul>
+						</details>
+						<!-- <button
+							v-for="status in commentStatus"
+							:key="status"
+							type="submit"
+							:class="`wl-btn wl-${status}`"
+							:disabled="comment.status === status"
+							@click="emit('status', { status, comment })"
+							v-text="locale[status]"
+						/> -->
 		
 						<button
 							v-if="isAdmin && !('rid' in comment)"
